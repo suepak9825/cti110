67a799239a3514a49b3f70268f3a2e9f2481012f
suepak9825 <160365873+suepak9825@users.noreply.github.com>
2024-02-18 03:32:22 -0500

    Add files via upload
    Tests knowledge of how to write code that collects information from user, processes information collected and display results to user.

---
+++ P1HW1_PakSue.py
@@ -0,0 +1,11 @@
+# Sue Pak
+# 2/17/2024
+# P1HW1
+# Write code that collects info from user, process them, and displays results to user
+user_num = int(input('Enter integer:\n'))
+print('You entered:', user_num)
+print(user_num, 'squared is', user_num * user_num)
+print('And', user_num, 'cubed is', user_num * user_num * user_num, '!!')
+user_num2 = int(input('Enter another integer:\n'))
+print(user_num, '+', user_num2, 'is', user_num + user_num2)
+print(user_num, '*', user_num2, 'is', user_num * user_num2)
