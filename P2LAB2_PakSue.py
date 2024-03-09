4b4dc6a8d2989d2207dbded77b3648b06acd8597
suepak9825 <160365873+suepak9825@users.noreply.github.com>
2024-03-09 03:28:41 -0500

    Add files via upload
    Test ability to edit and enhance exiting programs

--- P2LAB2_PakSue.py
+++ P2LAB2_PakSue.py
@@ -1,18 +1,32 @@
-# Sue Pak
-# 3/1/2024
-# P2LAB2
-# tests student's knowledge of how to write code that displays information to users
+#Sue Pak
+#3/9/2024
+#P2LAB2
+#Test ability to edit and enhance exiting programs
 
-num1 = float(input())
-num2 = float(input())
-num3 = float(input())
-num4 = float(input())
+print('This program calculates and displays travel expenses')
+print()
+print('Enter Budget:', end = ' ')
+budget = int(input())
+print()
+print('Enter your travel destination:', end = ' ')
+travel_destination = input()
+print()
+print('How much do you think you will spend on gas?', end = ' ')
+gas_estimate = int(input())
+print()
+print('Approximately, how much will you need for accomodation/hotel?', end = ' ')
+accomodation_hotel_fees = int(input())
+print()
+print('Last, how much do you need for food?', end = ' ')
+food_cost = int(input())
+print()
 
-product_nums = num1 * num2 * num3 * num4
-avg_nums = (num1 + num2 + num3 + num4)/4
-
-print(f'{product_nums:.0f}', end=' ')
-print(f'{avg_nums:.0f}')
-
-print(f'{product_nums:.3f}', end=' ')
-print(f'{avg_nums:.3f}')
+print(f"{'Travel Expenses':-^39}")
+print(f'Location:\t\t{travel_destination:}')
+print(f'Initial Budget:\t\t${float(budget):.2f}')
+print(f'Fuel:\t\t\t${float(gas_estimate):.2f}')
+print(f'Accomodation:\t\t${float(accomodation_hotel_fees):.2f}')
+print(f'Food:\t\t\t${float(food_cost):.2f}')
+print('----------------------------------------')
+print()
+print(f'Remaining Balance:\t${float(budget-(gas_estimate+accomodation_hotel_fees+food_cost)):.2f}')
